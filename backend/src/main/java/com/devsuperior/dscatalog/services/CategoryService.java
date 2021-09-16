8402bc7cc522e88b693af9c12361a7b0a0d5d57e
Rafael Vilarino <rovilarino@gmail.com>
2021-09-16 13:38:12 -0300

    h2 database

---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -0,0 +1,22 @@
+package com.devsuperior.dscatalog.services;
+
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository repository;
+
+    public CategoryService(CategoryRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Category> findAll(){
+        return repository.findAll();
+    }
+}
