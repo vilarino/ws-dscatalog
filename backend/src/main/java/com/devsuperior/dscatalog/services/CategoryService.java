a1061155101ac3d646e4b93ebb7a4f374f127b49
Rafael Vilarino <rovilarino@gmail.com>
2021-09-18 11:46:50 -0300

    POST new Category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -36,4 +36,11 @@ public class CategoryService {
 
         return new CategoryDTO(entity);
     }
+    @Transactional
+    public CategoryDTO insert(CategoryDTO dto) {
+        Category entity = new Category();
+        entity.setName(dto.getName());
+
+        return new CategoryDTO(repository.save(entity));
+    }
 }
