0076aebbf53f622e0640b112b4009ee9bbe11770
Rafael Vilarino <rovilarino@gmail.com>
2021-09-28 08:25:41 -0300

    Tests ProductRepository created, Factory created

---
+++ backend/src/test/java/com/devsuperior/dscatalog/tests/Factory.java
@@ -0,0 +1,29 @@
+package com.devsuperior.dscatalog.tests;
+
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.entities.Product;
+
+import java.time.Instant;
+
+public class Factory {
+    public static Product createProduct() {
+        Product product = new Product(
+                1L,
+                "PC Gamer Min",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                2250.0,
+                "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/23-big.jpg",
+                Instant.parse("2020-07-14T10:00:00Z")
+        );
+
+        product.getCategories().add(new Category(2L, "Electronics"));
+
+        return product;
+    }
+
+    public static ProductDTO createProductDTO(){
+        Product product = createProduct();
+        return new ProductDTO(product, product.getCategories());
+    }
+}
