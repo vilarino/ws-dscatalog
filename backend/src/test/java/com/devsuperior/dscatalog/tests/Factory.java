75aec8002a916ec036bfab8a68fadaa3f38e15f0
Rafael Vilarino <rovilarino@gmail.com>
2021-10-02 20:03:30 -0300

    Create category factory method

--- backend/src/test/java/com/devsuperior/dscatalog/tests/Factory.java
+++ backend/src/test/java/com/devsuperior/dscatalog/tests/Factory.java
@@ -17,7 +17,7 @@ public class Factory {
                 Instant.parse("2020-07-14T10:00:00Z")
         );
 
-        product.getCategories().add(new Category(2L, "Electronics"));
+        product.getCategories().add(createCategory());
 
         return product;
     }
@@ -25,5 +25,9 @@ public class Factory {
     public static ProductDTO createProductDTO(){
         Product product = createProduct();
         return new ProductDTO(product, product.getCategories());
+    }
+
+    public static Category createCategory(){
+        return new Category(2L, "Electronics");
     }
 }
