79a42aeb0126807836011470005910e0dcc932d9
Rafael Vilarino <rovilarino@gmail.com>
2021-10-06 09:19:59 -0300

    Bug: Altered class exception in ResourceExceptionHandler

--- backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -27,7 +27,7 @@ public class ResourceExceptionHandler {
     }
 
     @ExceptionHandler(DatabaseException.class)
-    public ResponseEntity<StandardError> database(ResourceNotFoundException e, HttpServletRequest request){
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
         HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError error = new StandardError();
         error.setTimestamp(Instant.now());
